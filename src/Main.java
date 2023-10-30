bcd564923c5933c4d5b3f882b00e86019c6c653b
MariaLarin <mashka2100@mail.ru>
2023-10-31 00:14:02 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        dz7.ApplePhone phone;
+        ApplePhone = new ApplePhone("999-999-999","Samsung S55", 100.123);
+        phone.receiveCall("Misha");
+        phone.info();
+    }
+}
